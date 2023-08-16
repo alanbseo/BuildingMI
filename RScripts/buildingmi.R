1d400b3c4e76473fe0ffc11661e08bfc074c7fbe
woogy114 <134253643+woogy114@users.noreply.github.com>
2023-08-16 10:32:15 +0900

    Create buildingmi.R
    전국건축물대장 파일 불러오기 및 구조별 연면적 합산

---
+++ RScripts/buildingmi.R
@@ -0,0 +1,50 @@
+install.packages("dplyr")
+install.packages("data.table")
+install.packages("bit64")
+install.packages("openxlsx")
+install.packages("tidyr")
+install.packages("stringr")
+library(dplyr)
+library(data.table)
+library("bit64")
+library(openxlsx)
+library(tidyr)
+library(stringr)
+
+buildingregister <- fread(file = "D:/국토교통부_건축물대장_표제부+(2023년+06월)/mart_djy_03.txt",header = FALSE,sep = "|" ,encoding = "unknown")
+print(buildingregister)
+buildingregister$year <- str_sub(buildingregister$V61, 1, 4)
+
+# 데이터프레임 정리
+summarized_data <- buildingregister %>%
+  group_by(year, V32) %>%
+  summarise(total_value = sum(V29))
+
+summarized_data$year <- as.character(summarized_data$year)
+summarized_data$V32 <- as.character(summarized_data$V32)
+summarized_data <- summarized_data %>%
+  filter(year != "" & V32 != "")
+
+print(summarized_data)
+
+# 피벗 테이블 생성
+pivot_data <- tidyr::pivot_wider(summarized_data, names_from = V32, values_from = total_value)
+
+# 피벗 테이블의 year 열을 행 이름으로 변경
+pivot_data <- as.data.frame(pivot_data)
+rownames(pivot_data) <- pivot_data$year
+
+pivot_data <- lapply(pivot_data, function(col) {
+  if(is.character(col)) iconv(col, to = "UTF-8", sub = "byte")
+  else col
+})
+
+# 엑셀 파일로 저장
+write.xlsx(pivot_data, "summarized_data.xlsx")
+##############################################################################################################################################################
+
+buildingregister <- read.csv("D:/국토교통부_건축물대장_표제부+(2023년+06월)/MART_DJY_202306.csv")
+print(buildingregister)
+
+
+
